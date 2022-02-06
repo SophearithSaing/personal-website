a2641e823283bc3d2fb65eff8db97d2ad32ce642
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2022-02-06 20:56:32 +0700

    Add ngx-highlightjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
 
+// Components
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+
+// Angular Material
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
@@ -14,7 +18,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
-import { HomeComponent } from './home/home.component';
+
+// Ngx-highlighjs
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 @NgModule({
   declarations: [
@@ -34,8 +40,16 @@ import { HomeComponent } from './home/home.component';
     MatIconModule,
     MatExpansionModule,
     MatTabsModule,
+    HighlightModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js'),
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  terminalConfig = `
+"profiles": {
+  "list": [
+    {
+      "guid": "{e794cec8-2fbc-4b3f-bca7-e1d8f3cd4fe3}",
+      "commandline": "%PROGRAMFILES%/git/usr/bin/bash.exe -i -l",
+      "icon": "%PROGRAMFILES%/Git/mingw64/share/git/git-for-windows.ico",
+      "name": "Git Bash",
+      "suppressApplicationTitle": true,
+      "startingDirectory": "%USERPROFILE%"
+    }
+  ]
+},
+"actions": [
+  {
+    "command": {
+      "action": "splitPane",
+      "split": "auto",
+      "profile": "{e794cec8-2fbc-4b3f-bca7-e1d8f3cd4fe3}"
+    },
+    "keys": "alt+d"
+  }
+]
+`;
   constructor() { }
 
   ngOnInit(): void {
